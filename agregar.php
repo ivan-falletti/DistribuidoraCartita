f38e9211b2130a5559bf6c6034e6350d9cfc1742
ivan-falletti <ivan_falletti@hotmail.com>
2017-11-22 13:22:44 -0300

    Added cart logic

--- agregar.php
+++ agregar.php
@@ -4,30 +4,13 @@
     session_start();
 
     // product details
-    $id = isset($_GET['id']) ?  $_GET['id'] : die;
-    $name = isset($_GET['name']) ?  $_GET['name'] : die;
-    $quantity  = isset($_GET['quantity']) ?  $_GET['quantity'] : die;
-    $user_id=1;
-    $created=date('Y-m-d H:i:s');
+    $productid = isset($_GET['id']) ?  $_GET['id'] : die;
+    $quantity = isset($_GET['quantity']) ?  $_GET['quantity'] : die;
+    $user_id = 1;
+    $created = date('Y-m-d H:i:s');
 
     // insert query
-    $query = "INSERT INTO cart_items SET product_id=?, quantity=?, user_id=?, created=?";
+    $conexion->query("INSERT INTO cart_items (product_id, quantity, user_id, created) VALUES ('$productid','$quantity', '$user_id','$created')");
 
-    // prepare query
-    $stmt = $con->prepare($query);
-
-    // bind values
-    $stmt->bindParam(1, $id);
-    $stmt->bindParam(2, $quantity);
-    $stmt->bindParam(3, $user_id);
-    $stmt->bindParam(4, $created);
-    
-    if($stmt->execute()) {
-        // if database insert succeeded
-        header('Location: productos.php?action=added&id=' . $id . '&name=' . $name);
-    }
-    else {
-        // if database insert failed
-        header('Location: productos.php?action=failed&id=' . $id . '&name=' . $name);
-    }
+    header('Location: index.php?action=added&id=' . $id);
 ?>
